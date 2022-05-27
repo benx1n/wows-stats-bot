4980b05a7740cc0e74ce5865847ab88f19a34e65
benx1n <1119809439@qq.com>
2022-05-27 19:45:59 +0800

    优化了提示

--- wws_info.py
+++ wws_info.py
@@ -76,7 +76,7 @@ async def get_AccountInfo(qqid,info):
                 else:
                     return '服务器参数似乎输错了呢'
             elif params:
-                print(params)
+                print('下面是本次请求的参数，如果遇到了问题，请将这部分连同报错日志一起发送给麻麻哦')
             else:
                 return '您似乎准备用游戏昵称查询水表，请检查参数中时候包含服务器和游戏昵称，以空格区分'
         else:
